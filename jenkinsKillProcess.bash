6908dcfbad71fab33ed8b5760222c70c20639f24
ckirabo <kirabo646@googlemail.com>
2019-01-10 13:10:20 +0000

    Update jenkinsKillProcess.bash

--- jenkinsKillProcess.bash
+++ jenkinsKillProcess.bash
@@ -3,7 +3,6 @@
 PROJECT="spring-boot-hello-world"
 mvn clean package
 INSTALL_FOLDER="${JENKINS_HOME}/${PROJECT}"
-mkdir -p $INSTALL_FOLDER
 
 # delete old process
 PID_FILE="${INSTALL_FOLDER}/PID"
@@ -11,12 +10,13 @@ OLD_PID=$(cat ${PID_FILE})
 kill -9 $OLD_PID
 
 # remove old files
-rm -rf $INSTALL_FOLDER/*
+rm -rf $INSTALL_FOLDER
+mkdir -p $INSTALL_FOLDER
 
 # copy in the new files
 cp target/hello-world*.jar ${INSTALL_FOLDER}/${PROJECT}.jar
 
 # run the new app
-BUILD_ID=dontKillMe java -jar ${INSTALL_FOLDER}/${PROJECT}.jar 2>&1&
+BUILD_ID=dontKillMe java -jar ${INSTALL_FOLDER}/${PROJECT}.jar &
 echo $!>${PID_FILE}
 disown
